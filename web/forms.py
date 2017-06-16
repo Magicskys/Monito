d05b901a10d666291c316739865fc6c188503295
master <987083065@qq.com>
2017-06-16 22:26:48 +0800

    master

--- web/forms.py
+++ web/forms.py
@@ -4,6 +4,7 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=10, widget=forms.TextInput(
-        attrs={'class': "form-control", 'id': "username", 'placeholder': "用户名"}))
+        attrs={'class': "form-control", 'id': "username", 'placeholder': "账号"}))
+
     password = forms.CharField(label="密码", max_length=10, widget=forms.PasswordInput(
         attrs={'class': "form-control", 'id': "password", 'placeholder': "密码"}))
